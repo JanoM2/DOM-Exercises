0369b21423ecbe42d5736d0bf0e4f60f8603a561
Jano <martinojano0@gmail.com>
2022-05-14 20:08:27 -0300

    Cree la carpeta assets en donde agregue dom img.png y sonido-de-sirena.mp3, tambien agregue los archivos alarma.js y reloj.js los cuales tienen funciones que importe al archivo codigo.js

--- codigo.js
+++ codigo.js
@@ -1,9 +1,13 @@
 import menu from "./dom/menu.js";
 import flecha from "./dom/flecha.js";
 import temas from "./dom/temaOscuroClaro.js";
+import reloj from "./dom/reloj.js";
+import alarma from "./dom/alarma.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
   menu(".btnHamb", ".modal-window", ".modal-content a");
   flecha(".flecha");
   temas(".theme", "[data-dark]");
+  reloj(".reloj1", ".reloj2", "#reloj");
+  alarma(".alarma1", ".alarma2", "assets/sonido-de-sirena.mp3");
 });
