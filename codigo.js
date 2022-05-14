33ac0cc7fae640957cf115be7b92fc6a2a20c535
Jano <martinojano0@gmail.com>
2022-05-14 19:25:29 -0300

    cree un archivo javascript llamado temaOscuroClaro hice una funcion para luego exportarla, ademas importe la funcion al archivo codigo.js

--- codigo.js
+++ codigo.js
@@ -1,7 +1,9 @@
 import menu from "./dom/menu.js";
 import flecha from "./dom/flecha.js";
+import temas from "./dom/temaOscuroClaro.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
   menu(".btnHamb", ".modal-window", ".modal-content a");
   flecha(".flecha");
+  temas(".theme", "[data-dark]");
 });
--- dom/menu.js
+++ dom/menu.js
@@ -11,36 +11,3 @@ export default function menu($btn, $panel, $menu) {
     }
   });
 }
-
-// // Tema Oscuro y Tema Claro
-
-// const $btn = document.querySelector(".theme");
-// const $selectores = document.querySelectorAll("[data-dark]");
-// let luna = "🌙",
-//   sol = "🌞";
-
-// $btn.addEventListener("click", () => {
-//   if ($btn.textContent === luna) {
-//     dark();
-//   } else {
-//     light();
-//   }
-// });
-
-// const dark = () => {
-//   $selectores.forEach((el) => el.classList.add("dark-mode"));
-//   $btn.textContent = sol;
-//   localStorage.setItem("modo oscuro", "on");
-// };
-
-// const light = () => {
-//   $selectores.forEach((el) => el.classList.remove("dark-mode"));
-//   $btn.textContent = luna;
-//   localStorage.setItem("modo oscuro", "off");
-// };
-
-// if (localStorage.getItem("modo oscuro") == "on") {
-//   dark();
-// } else {
-//   light();
-// }
