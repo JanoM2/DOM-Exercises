6264c75762e52a0913a290dc5b3cbb4145670920
Jano <martinojano0@gmail.com>
2022-05-16 16:26:18 -0300

    remove comments in cuentaRegresiva.js

--- dom/cuentaRegresiva.js
+++ dom/cuentaRegresiva.js
@@ -1,17 +1,13 @@
 const d = document;
 
 export default function cuentaAtras(div) {
-  let fechaCuentaAtras = new Date("Jan 01, 2023 00:00:00").getTime(); // obtengo el timeStamp de la fecha
+  let fechaCuentaAtras = new Date("Jan 01, 2023 00:00:00").getTime();
 
-  // Intervalo de un segundo
   let intervalo = setInterval(() => {
-    // el timeStamp de ahora en una variable
     let ahora = new Date().getTime();
 
-    // la distancia entre la fecha y ahora
     let diferencia = fechaCuentaAtras - ahora;
 
-    // calculos para los segundos, minutos, horas y dias
     let dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
     let horas = Math.floor(
       (diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -24,12 +20,10 @@ export default function cuentaAtras(div) {
     if (minutos < 10) minutos = "0" + minutos;
     if (segundos < 10) segundos = "0" + segundos;
 
-    // imprimir el valor
     d.querySelector(
       div
     ).innerHTML = `${dias}d ${horas}hs ${minutos}m ${segundos}s`;
 
-    // si la cuenta atras termina, imprimir un texto
     if (diferencia < 0) {
       clearInterval(intervalo);
       d.querySelector(div).innerHTML = "Cuenta Regresiva Finalizada";
